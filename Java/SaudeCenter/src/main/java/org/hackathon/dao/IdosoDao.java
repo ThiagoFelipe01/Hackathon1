5e7e1427c37270c7caf96c1b1139587878a664aa
thiago <tiagofelipe056@gmail.com>
2024-06-19 14:12:12 -0300

    adicionando as funcionalidades nas classes IdosoService, IdosoDao, Idoso

--- Java/SaudeCenter/src/main/java/org/hackathon/dao/IdosoDao.java
+++ Java/SaudeCenter/src/main/java/org/hackathon/dao/IdosoDao.java
@@ -1,8 +1,10 @@
 package org.hackathon.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.hackathon.model.Idoso;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IdosoDao {
     private Connection connection;
@@ -16,4 +18,31 @@ public class IdosoDao {
             throw new SQLException(e.getMessage());
         }
     }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void inserir (Idoso idoso) throws  SQLException {
+        String sql = "insert into idoso(nome,idade,cpf,telefone) values(?,?,?,?)";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, idoso.getNome());
+        ps.setInt(2, idoso.getIdade());
+        ps.setInt(3, idoso.getCpf());
+        ps.setInt(4, idoso.getTelefone());
+        ps.execute();
+    }
+
+
+    public void  atualizar(Idoso idoso) throws SQLException {
+        String sql = "update idoso set nome = ?, idade = ?, cpf = ?, telefone = ? where id = ?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, idoso.getNome());
+        ps.setInt(2, idoso.getIdade());
+        ps.setInt(3, idoso.getCpf());
+        ps.setInt(4, idoso.getTelefone());
+        ps.setInt(5, idoso.getId());
+        ps.execute();
+    }
+
 }
