8d049cb0da1a7a0ede8e1c35a6534feb584d54e2
wellingtonjoaquim <wellingtonjoaquim736@gmail.com>
2024-06-19 11:06:55 -0300

    Testando

--- Java/SaudeCenter/src/main/java/org/example/views/Historico.java
+++ Java/SaudeCenter/src/main/java/org/example/views/Historico.java
@@ -3,9 +3,4 @@ package org.example.views;
 import javax.swing.*;
 
 public class Historico {
-    private JLabel labelSome;
-
-    public Historico(JLabel labelSome) {
-        this.labelSome = labelSome;
-    }
 }
