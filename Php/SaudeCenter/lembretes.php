390b53381da7f1fc14ce69d76dd6a6c9f8ee7953
hazz-b <brenonmacedo@gmail.com>
2024-06-24 15:34:36 -0300

    Atualizações

--- Php/SaudeCenter/lembretes.php
+++ Php/SaudeCenter/lembretes.php
@@ -1,13 +1,17 @@
+<?php include 'templates/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Lembretes de Vacinação</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             margin: 20px;
+            
         }
         table {
             width: 100%;
@@ -97,3 +101,4 @@ if (!empty($agendamentos)) {
 
 </body>
 </html>
+<?php include 'templates/footer.php'; ?>
