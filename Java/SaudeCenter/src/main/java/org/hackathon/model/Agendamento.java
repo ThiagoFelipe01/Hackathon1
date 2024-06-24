6742f15e09fa3f85f0804ac8b6194939b2f760c2
Thiago da Silva Felipe <tiagofelipe056@gmail.com>
2024-06-24 14:49:00 -0300

    tentando fazer as exceções

--- Java/SaudeCenter/src/main/java/org/hackathon/model/Agendamento.java
+++ Java/SaudeCenter/src/main/java/org/hackathon/model/Agendamento.java
@@ -9,18 +9,21 @@ public class Agendamento {
     private Integer idIdoso;
     private Date dataAgendamento;
     private Time horario;
+    private String nomeIdoso;
 
-    public Agendamento(Integer idIdoso, Date dataAgendamento, Time horario) {
+    public Agendamento(Integer idIdoso, Date dataAgendamento, Time horario, String nomeIdoso) {
         this.idIdoso = idIdoso;
         this.dataAgendamento = dataAgendamento;
         this.horario = horario;
+        this.nomeIdoso = nomeIdoso;
     }
 
-    public Agendamento(Integer id, Integer idIdoso, Date dataAgendamento, Time horario) {
+    public Agendamento(Integer id, Integer idIdoso, Date dataAgendamento, Time horario, String nomeIdoso) {
         this.id = id;
         this.idIdoso = idIdoso;
         this.dataAgendamento = dataAgendamento;
         this.horario = horario;
+        this.nomeIdoso = nomeIdoso;
     }
 
     public Integer getId() {
@@ -55,8 +58,16 @@ public class Agendamento {
         this.horario = horario;
     }
 
+    public String getNomeIdoso() {
+        return nomeIdoso;
+    }
+
+    public void setNomeIdoso(String nomeIdoso) {
+        this.nomeIdoso = nomeIdoso;
+    }
+
     @Override
     public String toString() {
-        return "Id: " + id + " Id: " + idIdoso + " Data Visita: " + dataAgendamento + " Hora Visita: " + horario;
+        return "Id: " + id + " Nome: " + nomeIdoso + " Data Visita: " + dataAgendamento + " Hora Visita: " + horario;
     }
 }
